9c0836a4f7f4f861715a42e9932edc108cae4378
puneet/123001 <jainpuneet711@gmail.com>
2023-11-22 16:13:47 +0530

    Google signup and signup api

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,31 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs_clone/colors.dart';
 import 'package:google_docs_clone/repository/auth_repository.dart';
+import 'package:google_docs_clone/screens/home_screen.dart';
 import 'package:riverpod/riverpod.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
-  void signInWithGoogle(WidgetRef ref){
-    ref.read(authRepositoryProvider).signInWithGoogle();
+  void signInWithGoogle(WidgetRef ref, BuildContext context) async {
+    final sMessenger = ScaffoldMessenger.of(context);
+    final navigator = Navigator.of(context);
+    final errorModel =
+        await ref.read(authRepositoryProvider).signInWithGoogle();
+    if (errorModel.error != null) {
+      sMessenger.showSnackBar(SnackBar(
+        content: Text(errorModel.error!),
+      ));
+    } else {
+      ref.read(userProvider.notifier).update((state) => errorModel.data);
+      navigator
+          .push(MaterialPageRoute(builder: (context) => const HomeScreen()));
+    }
   }
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
-
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-            onPressed: () => signInWithGoogle(ref),
-            icon: Image.asset('assets/images/g-logo-2.png',height: 20,),
-            label: const Text('Sign in with Google',
-              style:  TextStyle(
-                color: kBlackColor,
-              ),),
-        style: ElevatedButton.styleFrom(
-          minimumSize: const Size(150, 150),
-          backgroundColor: kWhiteColor,
-        ),),
+          onPressed: () => signInWithGoogle(ref, context),
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
+          ),
+          label: const Text(
+            'Sign in with Google',
+            style: TextStyle(
+              color: kBlackColor,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(150, 150),
+            backgroundColor: kWhiteColor,
+          ),
+        ),
       ),
     );
   }
