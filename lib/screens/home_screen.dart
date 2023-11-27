0d36e5f46e27fa59d7436ae7f50254ccf22190c7
puneet/123001 <jainpuneet711@gmail.com>
2023-11-28 01:03:12 +0530

    socket.io and autosaving

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs_clone/colors.dart';
+import 'package:google_docs_clone/common/widgets/loader.dart';
+import 'package:google_docs_clone/models/document_model.dart';
 import 'package:google_docs_clone/repository/auth_repository.dart';
 import 'package:google_docs_clone/repository/document_repository.dart';
 import 'package:routemaster/routemaster.dart';
@@ -8,54 +10,84 @@ import 'package:routemaster/routemaster.dart';
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
-  void signOut(WidgetRef ref){
+  void signOut(WidgetRef ref) {
     ref.read(authRepositoryProvider).signOut();
     ref.read(userProvider.notifier).update((state) => null);
   }
 
-  void createDocument(BuildContext context,WidgetRef ref) async{
+  void createDocument(BuildContext context, WidgetRef ref) async {
     String token = ref.read(userProvider)!.token;
     final navigator = Routemaster.of(context);
     final snackbar = ScaffoldMessenger.of(context);
 
-    final errorModel = await ref.read(documentRepositoryProvider).createDocument(token);
-    if(errorModel.data!=null){
+    final errorModel =
+        await ref.read(documentRepositoryProvider).createDocument(token);
+    if (errorModel.data != null) {
       navigator.push('/document/${errorModel.data.id}');
-
-    }
-    else{
+    } else {
       snackbar.showSnackBar(SnackBar(content: Text(errorModel.error!)));
     }
-
   }
 
+  void navigateToDocument(BuildContext context , String documnetId){
+    Routemaster.of(context).push('/document/$documnetId');
+  }
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kWhiteColor,
-        elevation: 0,
-        actions: [
-          IconButton(
-            onPressed: () => createDocument(context, ref),
-            icon: const Icon(
-              Icons.add,
-              color: kBlackColor,
-            ),
-          ),
-          IconButton(
-              onPressed: () => signOut(ref),
+        appBar: AppBar(
+          backgroundColor: kWhiteColor,
+          elevation: 0,
+          actions: [
+            IconButton(
+              onPressed: () => createDocument(context, ref),
               icon: const Icon(
-                Icons.logout,
-                color: kRedColor,
-              ))
-        ],
-      ),
-      body: Center(
-        child: Text(
-          ref.watch(userProvider)!.email,
+                Icons.add,
+                color: kBlackColor,
+              ),
+            ),
+            IconButton(
+                onPressed: () => signOut(ref),
+                icon: const Icon(
+                  Icons.logout,
+                  color: kRedColor,
+                ))
+          ],
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: ref
+              .watch(documentRepositoryProvider)
+              .getDocument(ref.watch(userProvider)!.token),
+          builder: (context,snapshot){
+            if(snapshot.connectionState==ConnectionState.waiting){
+               return const Loader();
+            }
+
+            return Center(
+              child: Container(
+                width: 600,
+                margin: const EdgeInsets.only(top: 10),
+                child: ListView.builder(itemCount: snapshot.data!.data.length,
+                    itemBuilder: (context,index){
+                  DocumentModel document = snapshot.data!.data[index];
+
+                  return InkWell(
+                    onTap: () => navigateToDocument(context, document.id),
+                    child: SizedBox(
+                      height: 50,
+                      child: Card(
+                        child: Center(
+                          child: Text(document.title, style: const TextStyle(
+                            fontSize: 17,
+                          ),),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            );
+          },
+        ));
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
 const authRouter = require("./routes/auth");
 const documentRouter = require("./routes/document");
 
 const PORT = process.env.PORT | 3001;
 
 const app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 app.use(cors());
 app.use(express.json());
@@ -23,7 +26,30 @@ console.log("Connection successful");
 console.log(err);
 });
 
+io.on('connection', (socket) => {
+  socket.on('join', (documentId) => {
+    socket.join(documentId);
+    console.log("joined");
+  });
 
-app.listen(PORT, "0.0.0.0", ()=>{
+  socket.on("typing", (data) => {
+    // Broadcast to the room set on the socket
+    socket.broadcast.to(socket.documentRoom).emit("changes", data);
+  });
+
+  socket.on('save', (data) =>{
+    saveData(data);
+  });
+});
+
+const saveData  = async (data) => {
+let document = await Document.findById(data.room);
+document.content = data.delta;
+document = await document.save();
+
+}
+
+
+server.listen(PORT, "0.0.0.0", ()=>{
 console.log(`connected at port ${PORT}`);
 });
